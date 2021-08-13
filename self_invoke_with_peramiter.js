6d62e3bc6dce7a94cfcc160ee8cc1aa5a197d4a3
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:46:33 +0530

    Add files via upload

---
+++ self_invoke_with_peramiter.js
@@ -0,0 +1,18 @@
+// (function(x){
+// 	console.log(x);
+// })("Hello, World!");
+
+// function num(a,b){
+//     c=a+b
+//     console.log(c)
+// }
+// num(4,6)
+
+
+
+(function() {
+    var foo = 3;
+    console.log(foo);
+})();
+console.log("kavita")
+  
