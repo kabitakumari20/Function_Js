df2e21602364cb7cfb80ceee7f4462b8d60ef72d
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:50:12 +0530

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,24 @@
+// var readlineSync=require("readline-sync");
+// var slary=readlineSync.questionInt("enter the slary=")
+// var year=readlineSync.questionInt("enter the year=")
+// if (year>5){
+//     var a=slary/5
+//     console.log(a , "this is the bouns")
+// }
+// else{
+//     console.log("no bonus")
+// }
+// console.log(sum(4,5))
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum(4,5))
+
+
+var a="9"
+var b=8
+console.log(`this is my total number ${a+b}.`)
+
+// console.log(`my name is kavita
+//                 i am from bihar`)
+
