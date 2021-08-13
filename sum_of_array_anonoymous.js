e70b350e5980e07f8117f12aafc01f1bc6e794e1
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:45:54 +0530

    Add files via upload

---
+++ sum_of_array_anonoymous.js
@@ -0,0 +1,11 @@
+var sum=function(){
+    var array=[1,2,3,4,5,6,7];
+    var i=0
+    var sum=0
+    while (i<array.length){
+        sum=sum+array[i]
+        i=i+1
+    }
+    console.log(sum)
+}
+sum()
